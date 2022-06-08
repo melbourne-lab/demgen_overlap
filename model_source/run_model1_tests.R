ed219c3178a923828cb61c0f94f48523be2e0856
swnordstrom <swnordstrom@email.wm.edu>
2022-06-08 17:37:16 -0600

    add another test of changing breeding value variances

--- model_source/run_model1_tests.R
+++ model_source/run_model1_tests.R
@@ -24,8 +24,7 @@ print(paste('Plain simulation run for', pars$timesteps, 'generations and',
 ## Look at population size over time
 table(test.sim$gen)
 ## Look at one longer-lived individual
-test.sim %>%
-  filter(i %in% 9)
+test.sim %>% filter(i %in% 3)
 # yes - fecundity changes in each time step! nice.
 # s_i changes because of changing population size
 ## Look at variance in r_i to see if it's actually updating
@@ -53,6 +52,12 @@ test.sim %>%
             b.bar0 = mean(b_i[age == 1]),
             b.bar1 = mean(b_i[age >  1]))
 # Adaptation is present!
+
+# Look at breeding value variance
+test.sim %>%
+  group_by(gen) %>%
+  summarise(b.var = var(b_i))
+# wow holy cow this gets tiny very quickly...
 
 ##### Run a simulation with changing theta
 
@@ -124,4 +129,40 @@ test.sim %>%
 test.sim %>%
   group_by(gen) %>%
   summarise(r = mean(s_i * r_i * 2))
+
+
+##### Try a simulation in stable, theta = 0 conditions to track genetic variance
+
+pars = data.frame(
+  n.pop0 = 100, s.max = 0.9, r = (1.2 / (0.9)) - 1, wfitn = 2,
+  sig.a = sqrt(0.5), sig.e = sqrt(0.5), alpha = 0,
+  timesteps = 20
+)
+
+set.seed(451)
+
+test.sim = sim(params = pars, theta.t = 0, init.rows = 1e5)
+
+print(paste('Neutral sims run for', pars$timesteps, 'generations with',
+            length(unique(test.sim$i)), 'individuals'))
+
+# Look at breeding values over time
+test.sim %>%
+  ggplot(aes(x = gen, y = b_i)) +
+  geom_point(aes(colour = age), position = position_jitter(width = 0.1)) +
+  scale_colour_viridis_c()
+test.sim %>%
+  mutate(adult = ifelse(age > 1, 'adult', 'offspring')) %>%
+  ggplot(aes(x = gen, y = b_i)) +
+  geom_point(alpha = 0.5, position = position_jitter(width = 0.2)) +
+  facet_wrap(~ adult) 
+test.sim %>%
+  group_by(gen) %>%
+  summarise(b.var = var(b_i)) %>%
+  plot(type = 'l')
+test.sim %>%
+  group_by(gen, adult = ifelse(age > 1, 'adult', 'offspring')) %>%
+  summarise(b.var = var(b_i)) %>%
+  ggplot(aes(x = gen, y = b.var)) +
+  geom_line(aes(colour = adult))
 
